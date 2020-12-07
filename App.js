c565b410c58989032b855c7ddc2e225582eac271
raphael-guedj <raphael.guedj1@gmail.com>
2020-12-07 12:23:59 +0100

    commit signup design

--- App.js
+++ App.js
@@ -5,72 +5,83 @@ import { LogBox } from "react-native";
 LogBox.ignoreAllLogs(true);
 import { Provider } from "react-redux";
 import { createStore, combineReducers } from "redux";
+import { AppLoading } from "expo";
+import SignUpScreen from "./screens/signupscreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  OpenSans_400Regular,
+  OpenSans_300Light,
+  OpenSans_800ExtraBold,
+} from "@expo-google-fonts/open-sans";
 import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
-const store = createStore(combineReducers({}));
+// const store = createStore(combineReducers({}));
 
-const PageTab = () => {
-  return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
+// const PageTab = () => {
+//   return (
+//     <Tab.Navigator
+//       initialRouteName="Home"
+//       screenOptions={({ route }) => ({
+//         tabBarIcon: ({ focused, color, size }) => {
+//           let iconName;
 
-          if (route.name === "Home") {
-            iconName = "ios-search";
-          } else if (route.name === "Forky") {
-            iconName = "ios-list";
-          } else if (route.name === "Profil") {
-            iconName = "md-person";
-          }
-          return <Ionicons name={iconName} size={34} color={color} />;
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: "#418581",
-        inactiveTintColor: "#f5f5c3",
-        style: {
-          backgroundColor: "#FAFAE0",
-          paddingVertical: 8,
-        },
-      }}
-    >
-      <Tab.Screen name="Déjeunez" component={HomeScreen} />
-      <Tab.Screen name="Mes Forky" component={LunchScreen} />
-      <Tab.Screen name="Mon Profil" component={ProfilScreen} />
-    </Tab.Navigator>
-  );
-};
+//           if (route.name === "Home") {
+//             iconName = "ios-search";
+//           } else if (route.name === "Forky") {
+//             iconName = "ios-list";
+//           } else if (route.name === "Profil") {
+//             iconName = "md-person";
+//           }
+//           return <Ionicons name={iconName} size={34} color={color} />;
+//         },
+//       })}
+//       tabBarOptions={{
+//         activeTintColor: "#418581",
+//         inactiveTintColor: "#f5f5c3",
+//         style: {
+//           backgroundColor: "#FAFAE0",
+//           paddingVertical: 8,
+//         },
+//       }}
+//     >
+//       <Tab.Screen name="Déjeunez" component={HomeScreen} />
+//       <Tab.Screen name="Mes Forky" component={LunchScreen} />
+//       <Tab.Screen name="Mon Profil" component={ProfilScreen} />
+//     </Tab.Navigator>
+//   );
+// };
 
 export default function App() {
-  return (
-    <Provider store={store}>
+  let [fontsLoaded] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_300Light,
+    OpenSans_800ExtraBold,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      // <Provider store={store}>
       <NavigationContainer>
         {/* Stack navigation pour les premières routes...Conditions a renseigner avec token ou pas aprés inscription en BDD */}
-        <Stack.Navigator initialRouteName="Init">
-          <Stack.Screen
-            name="S'inscrire avec votre Email"
-            component={InitScreen}
-          />
-          <Stack.Screen
-            name="S'inscrire avec votre LinkedIn"
-            component={InitScreen}
-          />
-          <Stack.Screen name="Se connecter" component={InitScreen} />
+        <Stack.Navigator
+          initialRouteName="Valider l'inscription"
+          headerMode="none"
+        >
           <Stack.Screen name="Valider l'inscription" component={SignUpScreen} />
-          <Stack.Screen name="Valider " component={SignInScreen} />
-          <Stack.Screen name="Déjeunez" component={PageTab} />
+          {/* <Stack.Screen name="Valider " component={SignInScreen} />
+            <Stack.Screen name="Déjeunez" component={PageTab} /> */}
         </Stack.Navigator>
       </NavigationContainer>
-    </Provider>
-  );
+      // </Provider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
