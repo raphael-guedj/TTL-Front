d9a04b7180346ab628f66aa42dae0ca6f30b5205
raphael-guedj <raphael.guedj1@gmail.com>
2020-12-14 10:07:51 +0100

    add notifscreen

---
+++ Screens/NotifScreen.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { Button, Input, CheckBox } from "react-native-elements";
+import { useIsFocused } from "@react-navigation/native";
+import { connect } from "react-redux";
+import DropDownPicker from "react-native-dropdown-picker";
+import Textarea from "react-native-textarea";
+import InputScrollView from "react-native-input-scroll-view";
+
+import { Feather, Entypo, MaterialIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
+
+const NotifScreen = () => {};
+
+export default EditProfilScreen;
